97e5292dcafa68990b136197b0411dcf05e47e5f
yanjq <yanjq@realbull.net>
2017-05-16 15:10:50 +0800

    新增listview官方demo

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import {AppRegistry} from 'react-native';
-import welcome from './period_1/scrollView/listView1';
+// import welcome from './period_1/scrollView/listView1';
+import welcome from './period_1/scrollView/listView2';
 
 AppRegistry.registerComponent('app', () => welcome);
--- period_1/scrollView/listView1.js
+++ period_1/scrollView/listView1.js
@@ -6,11 +6,13 @@ export default class welcome extends Component{
 	constructor(props){
 		super(props);
 		var ds = new ListView.DataSource({
-			rowHasChanged:(r1,r2) => r1 != r2
+			rowHasChanged:(r1,r2) => r1 !== r2,
+			sectionHeaderHasChanged:(s1,s2) => s1 !== s2
 		});
 		this.state = {
 			dataSource:ds,
-			data:["h","e","l","l","o"]
+			// data:["h","e","l","l","o"]
+			data:{a:['h','m'],b:['e','j'],c:['l','f'],d:['l','i'],e:['o','h']}
 		}
 	}
 
@@ -19,19 +21,46 @@ export default class welcome extends Component{
 			<View style = {styles.container}>
 				<ListView 
 					style = {{marginTop:20}}
-					dataSource = {this.state.dataSource.cloneWithRows(this.state.data)}
-					renderRow = {(rowData,sectionId,rowId) => this._renderRow(rowData,rowId)}
+					// dataSource = {this.state.dataSource.cloneWithRows(this.state.data)}
+					dataSource = {this.state.dataSource.cloneWithRowsAndSections(this.state.data)}
+					// renderRow = {(rowData,sectionId,rowId) => this._renderRow(rowData,rowId)}
+					renderRow = {(rowData,sectionId,rowId) => this._renderRow(rowData,rowId,sectionId)}
 					showsVerticalScrollIndictor = {false}
 				/>
 			</View>
 		);
 	}
 
-	_renderRow(rowData,rowId){
+	_renderRow(rowData,rowId,sectionId){
 		return (
-			<Text>{rowData+ ' ' + rowId}</Text>
+			<Text> { rowData+ ' ' + rowId+' '+sectionId } </Text>
 		);
 	}
+
+	_fetch(url){
+		try {
+			let repsonse = await fetch(url);
+			let json = await response.json();
+			return json.result;
+		} catch(e) {
+			console.log("网络请求失败",e);
+		}
+	}
+
+   _combineData(){
+	 	try {
+	 		// statements
+	 		var searchData = {};
+	 		let service_data = this._fetch(service);
+	 		searchData.service = service_data.service_list;
+	 		
+
+	 	} catch(e) {
+	 		// statements
+	 		console.log('合共数据失败',e);
+	 	}
+	}
+
 }
 
 const styles = StyleSheet.create({
