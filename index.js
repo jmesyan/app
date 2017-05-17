3e74751b5c788bb03c9bb8237834b9d1836f001b
jmesyan <jieqiang1017@qq.com>
2017-05-17 22:11:07 +0800

    补充

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 import {AppRegistry} from 'react-native';
+// import  welcome from './01_flex/flex1';
+// import  welcome from './01_flex/flex2';
+// import  welcome from './02_fetch/fetch1';
+// import  welcome from './03_library/tabNavi';
+// import  welcome from './03_library/tabNavTop';
+// import  welcome from './03_library/banner';
+// import  welcome from './03_library/banner2';
+// import  welcome from './03_library/banner3';
+// import  welcome from './03_library/banner4';
+// import  welcome from './04_framework/ele-button';
+// import  welcome from './04_framework/ele-heading';
+// import  welcome from './04_framework/ele-avatar';
+// import  welcome from './05_scroll/scroll1';
+// import  welcome from './05_scroll/scroll2';
 // import welcome from './period_1/scrollView/listView1';
 import welcome from './period_1/scrollView/listView2';
 
