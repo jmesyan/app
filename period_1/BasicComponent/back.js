78e0cd4f18b2aa9662d5cb4ef0f5c7feb621c245
yanjq <yanjq@realbull.net>
2017-05-25 09:24:27 +0800

    退出键

--- period_1/BasicComponent/back.js
+++ period_1/BasicComponent/back.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, {Component}from 'react';
 import {
   Platform,
   TouchableOpacity,
   StyleSheet,
   Navigator,
   View,
-  Text
-	// BackAndroid
+  Text,
+	BackAndroid
 } from 'react-native';
 
 
@@ -16,10 +16,51 @@ const defaultRoute = {
   component: Splash
 };
 
-export default class welcome extends React.Component{
-	 render(){
-    
+var _navigator;
+export default class welcome extends Component{
+
+   componentWillMount() {
+    if (Platform.OS === 'android') {
+      BackAndroid.addEventListener('hardwareBackPress', this.onBackAndroid.bind(this));
+    }
+  }
+  componentWillUnmount() {
+    if (Platform.OS === 'android') {
+      BackAndroid.removeEventListener('hardwareBackPress', this.onBackAndroid.bind(this));
+    }
+  }
+
+  onBackAndroid(){
+    console.log(11111);
+    const nav = _navigator;
+    console.log(nav);
+    // return true;
+    const routers = nav.getCurrentRoutes();
+    console.log(routers);
+    if (routers.length > 1) {
+      console.log(22222);
+      nav.pop();
+      return false;
+    }
+    console.log(33333);
+    return false;
+  }
+
+   _renderScene(route, navigator){
+      _navigator = navigator;
+      let Mycomponent=route.component;
+      return (
+          <Mycomponent {...route.params} navigator={navigator} />
+        );
    }
+
+
+   render(){
+    return (
+      <Navigator initialRoute={defaultRoute} renderScene={this._renderScene} />
+    );
+   }
+
 }
 
 const styles = StyleSheet.create({
